b1bd91ec522193bf006103c57183692b0cbf5f1e
artimiti <toingha@mail.ru>
2025-06-29 14:18:46 +0300

    del

--- js/view/updateResultsView.js
+++
@@ -1,5 +0,0 @@
-function updateResultsView(results) {
-    document.querySelector('#total-percent').innerHTML = results.rate * 100 + '%'
-}
-
-export default updateResultsView;
--- js/view/utils.js
+++
@@ -1,5 +0,0 @@
-function updateMinPercents(data) {
-    document.querySelector('#percents-from').innerText = data.minPaymentPercents * 100 + '%';
-}
-
-export { updateMinPercents };
